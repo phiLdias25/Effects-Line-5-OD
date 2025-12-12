6399d4df027093bc872624da81efb357a8339787
phiLdias25 <philipe.dias03@gmail.com>
2025-12-11 23:46:58 -0300

    Adição de novas bases

--- codigo_artigo.R
+++ codigo_artigo.R
@@ -52,4 +52,92 @@ od_2007_filtrada <- od_2007 |>
         DURACAO, # Duração da Viagem (MINUTOS)
         MODOPRIN, # Modo Principal da Viagem
         DISTANCIA, # Distância da Viagem (Linha reta entre a coord de origem e coord de destino)
+        ZONA_O, # Zona de Origem da Viagem
+        CO_O_X, # Coordenada X da Origem da Viagem
+        CO_O_Y, # Coordenada Y da Origem da Viagem
+        ZONA_D, # Zona de Destino da Viagem
+        CO_D_X, # Coordenada X do Destino da Viagem
+        CO_D_Y, # Coordenada Y do Destino da Viagem
+        MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
+        MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
+    )
+
+od_2017_filtrada <- od_2017 |>
+    select(
+        ZONA, # Zona do domicílio
+        MUNI_DOM, # Município do domicílio
+        CO_DOM_X, # Coordenada X do domicílio
+        CO_DOM_Y, # Coordenada Y do domicílio
+        ID_DOM, # Identificação do domicílio
+        TIPO_DOM, # Tipo do domicílio: 1 - Particular; 2 - Coletivo
+        CONDMORA, # Condição de moradia: 1 - Alugada; 2 - Própria; 3 - Cedida, 4 - Outros; 5 - Não respondeu
+        CRITERIOBR, # Critério de Classificação Econômica Brasil
+        ID_PESS, # Identificação da pessoa
+        SIT_FAM, # Situação Familiar: 1 - Responsável; 2 - Cônjuge/companheiro(a); 3 - Filho(a)/Enteado(a); 4 - Outro Parente; 5 - Agregado; 6 - Empregado Residente; 7 - Parente do Empregado
+        IDADE,
+        SEXO,
+        ESTUDA, # 1 - Não; 2 - Creche/Pré-Escola; 3 - Ensino Fundamental; 4 - Ensino Médio; 6 - Ensino Superior; 7 - Outros
+        GRAU_INS, # Grau de Instrução: 1 - Não Alfabetizado/ Fund 1 Incompleto ; 2 - Fund 1 Completo/Fund 2 Incompleto; 3 - Fund 2 Completo/ Médio Incompleto; 4 - Médio Completo/ Superior Incompleto; 5 - Superior Completo
+        CD_ATIVI, # Condição de atividade: 1 - Trabalho Regular; 2 - Bico; 3 - Licença Médica; 4 - Aposentado/Pensionista; 5 - Desempregado; 6 - Nunca trabalhou; 7 - Dona de Casa; 8 - Estudante
+        VL_REN_I, # Renda Individual
+        ZONATRA1, # Zona de Trabalho 1
+        MUNITRA1, # Município de Trabalho 1
+        CO_TR1_X, # Coordenada X do Trabalho 1
+        CO_TR1_Y, # Coordenada Y do Trabalho 1
+        OCUP1, # Ocupação do Trabalho 1
+        SETOR1, # Setor do Trabalho 1
+        VINC1, # Vínculo do Trabalho 1
+        TOT_VIAG, # Total de Viagens do Indivíduo
+        MODO1, # Modo de Transporte da Viagem 1
+        DURACAO, # Duração da Viagem (MINUTOS)
+        MODOPRIN, # Modo Principal da Viagem
+        DISTANCIA, # Distância da Viagem (Linha reta entre a coord de origem e coord de destino)
+        ZONA_O, # Zona de Origem da Viagem
+        CO_O_X, # Coordenada X da Origem da Viagem
+        CO_O_Y, # Coordenada Y da Origem da Viagem
+        ZONA_D, # Zona de Destino da Viagem
+        CO_D_X, # Coordenada X do Destino da Viagem
+        CO_D_Y, # Coordenada Y do Destino da Viagem
+        MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
+        MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
+    )
+
+od_2023_filtrada <- od_2023 |>
+    select(
+        ZONA, # Zona do domicílio
+        MUNI_DOM, # Município do domicílio
+        CO_DOM_X, # Coordenada X do domicílio
+        CO_DOM_Y, # Coordenada Y do domicílio
+        ID_DOM, # Identificação do domicílio
+        TIPO_DOM, # Tipo do domicílio: 1 - Particular; 2 - Coletivo
+        CONDMORA, # Condição de moradia: 1 - Alugada; 2 - Própria; 3 - Cedida, 4 - Outros; 5 - Não respondeu
+        CRITERIOBR, # Critério de Classificação Econômica Brasil
+        ID_PESS, # Identificação da pessoa
+        SIT_FAM, # Situação Familiar: 1 - Responsável; 2 - Cônjuge/companheiro(a); 3 - Filho(a)/Enteado(a); 4 - Outro Parente; 5 - Agregado; 6 - Empregado Residente; 7 - Parente do Empregado
+        IDADE,
+        SEXO,
+        ESTUDA, # 1 - Não; 2 - Creche/Pré-Escola; 3 - Ensino Fundamental; 4 - Ensino Médio; 6 - Ensino Superior; 7 - Outros
+        GRAU_INS, # Grau de Instrução: 1 - Não Alfabetizado/ Fund 1 Incompleto ; 2 - Fund 1 Completo/Fund 2 Incompleto; 3 - Fund 2 Completo/ Médio Incompleto; 4 - Médio Completo/ Superior Incompleto; 5 - Superior Completo
+        CD_ATIVI, # Condição de atividade: 1 - Trabalho Regular; 2 - Bico; 3 - Licença Médica; 4 - Aposentado/Pensionista; 5 - Desempregado; 6 - Nunca trabalhou; 7 - Dona de Casa; 8 - Estudante
+        VL_REN_I, # Renda Individual
+        ZONATRA1, # Zona de Trabalho 1
+        MUNITRA1, # Município de Trabalho 1
+        CO_TR1_X, # Coordenada X do Trabalho 1
+        CO_TR1_Y, # Coordenada Y do Trabalho 1
+        DS_OCUP_TR, # Ocupação do Trabalho 1
+        SETOR1, # Setor do Trabalho 1
+        VINC1, # Vínculo do Trabalho 1
+        TOT_VIAG, # Total de Viagens do Indivíduo
+        MODO1, # Modo de Transporte da Viagem 1
+        DURACAO, # Duração da Viagem (MINUTOS)
+        MODOPRIN, # Modo Principal da Viagem
+        DISTANCIA, # Distância da Viagem (Linha reta entre a coord de origem e coord de destino)
+        ZONA_O, # Zona de Origem da Viagem
+        CO_O_X, # Coordenada X da Origem da Viagem
+        CO_O_Y, # Coordenada Y da Origem da Viagem
+        ZONA_D, # Zona de Destino da Viagem
+        CO_D_X, # Coordenada X do Destino da Viagem
+        CO_D_Y, # Coordenada Y do Destino da Viagem
+        MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
+        MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
     )
