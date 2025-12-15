bfb3765fa41653e69abd643dd290a8e7e18fed47
phiLdias25 <philipe.dias03@gmail.com>
2025-12-15 18:12:47 -0300

    Criação da variável indicadora de viagem ao centro expandido de SP e junção das bases

--- codigo_artigo.R
+++ codigo_artigo.R
@@ -14,7 +14,7 @@ library(stargazer)
 
 ##### Importando base de dados #####
 
-od_1997 <- import('OD97Zona.dbf')
+#od_1997 <- import('OD97Zona.dbf')
 
 od_2007 <- import('OD_2007_v2d.dbf')
 
@@ -24,60 +24,60 @@ od_2023 <- import('Banco2023_divulgacao_190225.dbf')
 
 ##### Filtrando as variáveis importantes de cada base #####
 
-od_1997_filtrada <- od_1997 |>
-    select(
-        ZONA, # Zona do domicílio
-        MUNI_DOM, # Município do domicílio
-        ID_DOM, # Identificação do domicílio
-        TIPO_DOM, # Tipo do domicílio: 1 - Particular; 2 - Coletivo; 3 - Favela
-        CONDMORA, # Condição de moradia: 1 - Alugada; 2 - Própria; 3 - Cedida, 4 - Outros; 5 - Não respondeu
-        ABIPEME, # Critério de Classificação Econômica Brasil (antiga, chamada de ABIPEME)
-        ID_PESS, # Identificação da pessoa
-        SIT_FAM, # Situação Familiar: 1 - Responsável; 2 - Cônjuge/companheiro(a); 3 - Filho(a)/Enteado(a); 4 - Outro Parente; 5 - Empregado Residente; 6 - Visitante não residente
-        IDADE,
-        SEXO,
-        SE_ESTUD, # 1 - Não; 2 - Creche/Pré-Escola; 3 - Ensino Básico/Fundamental/Médio; 4 - Outros
-        GRAU_INS, # Grau de Instrução: 1 - Não Alfabetizado; 2 - Pré-Escola; 3 - Fundamental Incompleto; 4 - Fundamental Completo; 5 - Médio Incompleto; 6 - Médio Completo; 7 - Superior Incompleto; 8 - Superior Completo
-        CD_ATIVI, # Condição de atividade: 1 - Trabalho Regular; 2 - Bico; 3 - Licença Médica; 4 - Desempregado; 5 - Aposentado/Pensionista; 6 - Nunca trabalhou; 7 - Dona de Casa; 8 - Estudante
-        VL_REN_I, # Renda Individual
-        ZONATRA1, # Zona de Trabalho 1
-        MUNITRA1, # Município de Trabalho 1
-        SET_ATIV, # Setor do Trabalho 1
-        OCUP_PRI, # Vínculo do Trabalho 1
-        MODO1, # Modo de Transporte da Viagem 1
-        DURACAO, # Duração da Viagem (MINUTOS)
-        MODOPRIN, # Modo Principal da Viagem
-        ZONA_O, # Zona de Origem da Viagem
-        ZONA_D, # Zona de Destino da Viagem
-        MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Outros
-        MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Outros
-    ) |>
-    mutate(
-        ano = 1997,
-        TOT_VIAG = NA,
-        CO_DOM_X = NA,
-        CO_DOM_Y = NA,
-        CO_TR1_X = NA,
-        CO_TR1_Y = NA,
-        DISTANCIA = NA,
-        CO_O_X = NA,
-        CO_O_Y = NA,
-        CO_D_X = NA,
-        CO_D_Y = NA,
-        ID_DOM = as.character(ID_DOM)
-    )
+#od_1997_filtrada <- od_1997 |>
+#    select(
+#        ZONA, # Zona do domicílio
+#        MUNI_DOM, # Município do domicílio
+#        ID_DOM, # Identificação do domicílio
+#        TIPO_DOM, # Tipo do domicílio: 1 - Particular; 2 - Coletivo; 3 - Favela
+#        CONDMORA, # Condição de moradia: 1 - Alugada; 2 - Própria; 3 - Cedida, 4 - Outros; 5 - Não respondeu
+#        ABIPEME, # Critério de Classificação Econômica Brasil (antiga, chamada de ABIPEME)
+#        ID_PESS, # Identificação da pessoa
+#        SIT_FAM, # Situação Familiar: 1 - Responsável; 2 - Cônjuge/companheiro(a); 3 - Filho(a)/Enteado(a); 4 - Outro Parente; 5 - Empregado Residente; 6 - Visitante não residente
+#        IDADE,
+#       SEXO,
+#        SE_ESTUD, # 1 - Não; 2 - Creche/Pré-Escola; 3 - Ensino Básico/Fundamental/Médio; 4 - Outros
+#        GRAU_INS, # Grau de Instrução: 1 - Não Alfabetizado; 2 - Pré-Escola; 3 - Fundamental Incompleto; 4 - Fundamental Completo; 5 - Médio Incompleto; 6 - Médio Completo; 7 - Superior Incompleto; 8 - Superior Completo
+#        CD_ATIVI, # Condição de atividade: 1 - Trabalho Regular; 2 - Bico; 3 - Licença Médica; 4 - Desempregado; 5 - Aposentado/Pensionista; 6 - Nunca trabalhou; 7 - Dona de Casa; 8 - Estudante
+#        VL_REN_I, # Renda Individual
+#        ZONATRA1, # Zona de Trabalho 1
+#        MUNITRA1, # Município de Trabalho 1
+#        SET_ATIV, # Setor do Trabalho 1
+#        OCUP_PRI, # Vínculo do Trabalho 1
+#        MODO1, # Modo de Transporte da Viagem 1
+#        DURACAO, # Duração da Viagem (MINUTOS)
+#        MODOPRIN, # Modo Principal da Viagem
+#        ZONA_O, # Zona de Origem da Viagem
+#        ZONA_D, # Zona de Destino da Viagem
+#        MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Outros
+#        MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Outros
+#    ) |>
+#    mutate(
+#        ano = 1997,
+#        TOT_VIAG = NA,
+#       CO_DOM_X = NA,
+#       CO_DOM_Y = NA,
+#        CO_TR1_X = NA,
+#        CO_TR1_Y = NA,
+#       DISTANCIA = NA,
+#        CO_O_X = NA,
+#        CO_O_Y = NA,
+#        CO_D_X = NA,
+#        CO_D_Y = NA,
+#        ID_DOM = as.character(ID_DOM)
+#    )
 
 # Problemas: 1997 não possui coordenadas, critério econômico antigo, grau de instrução limitado, sem total de viagens, ocupação do trabalho 1 e setor do trabalho 1 diferentes
 
 ## Trocando nomes diferentes da base de 1997
 
-od_1997_filtrada <- od_1997_filtrada |>
-    rename(
-        CRITERIOBR = ABIPEME,
-        ESTUDA = SE_ESTUD,
-        VINC1 = OCUP_PRI,
-        SETOR1 = SET_ATIV
-    )
+#od_1997_filtrada <- od_1997_filtrada |>
+#    rename(
+#        CRITERIOBR = ABIPEME,
+#        ESTUDA = SE_ESTUD,
+#       VINC1 = OCUP_PRI,
+#       SETOR1 = SET_ATIV
+#    )
 
 od_2007_filtrada <- od_2007 |>
     select(
@@ -159,7 +159,13 @@ od_2017_filtrada <- od_2017 |>
         MOTIVO_O, # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
         MOTIVO_D # Motivo da Viagem de Origem: 1 - Trabalho/Indústria; 2 - Trabalho/Comércio; 3 - Trabalho/Serviços; 4 - Educação; 5 - Compras; 6 - Saúde; 7 - Lazer; 8 - Residência; 9 - Procurar Emprego; 10 - Assuntos Pessoais
     ) |>
-    mutate(ano = 2017)
+    mutate(
+        ano = 2017,
+        CO_O_X_SIRGAS = CO_O_X,
+        CO_O_Y_SIRGAS = CO_O_Y,
+        CO_D_X_SIRGAS = CO_D_X,
+        CO_D_Y_SIRGAS = CO_D_Y
+    )
 
 
 od_2023_filtrada <- od_2023 |>
@@ -205,17 +211,20 @@ od_2023_filtrada <- od_2023 |>
         ano = 2023,
         ID_PESS = as.character(ID_PESS),
         VL_REN_I = as.numeric(VL_REN_I),
-        OCUP1 = DS_OCUP_TR
+        CO_O_X_SIRGAS = CO_O_X,
+        CO_O_Y_SIRGAS = CO_O_Y,
+        CO_D_X_SIRGAS = CO_D_X,
+        CO_D_Y_SIRGAS = CO_D_Y
     )
 
 ##### Arrumando diferenças entre as pesquisas de cada base #####
 
 ### Em 1997 e 2007, a variável de tipo de domicílio engloba favelas como um tipo diferente, enquanto as outras não fazem a separação. Vamos separá-las como domicílios particulares ###
 
-od_1997_filtrada <- od_1997_filtrada |>
-    mutate(
-        TIPO_DOM = ifelse(TIPO_DOM == 3, 1, TIPO_DOM)
-    )
+#od_1997_filtrada <- od_1997_filtrada |>
+#    mutate(
+#        TIPO_DOM = ifelse(TIPO_DOM == 3, 1, TIPO_DOM)
+#    )
 
 od_2007_filtrada <- od_2007_filtrada |>
     mutate(
@@ -224,26 +233,174 @@ od_2007_filtrada <- od_2007_filtrada |>
 
 ### Em 1997, o grau de instrução estava mais detalhado. Vamos adequá-lo às outras pesquisas ###
 
-od_1997_filtrada <- od_1997_filtrada |>
+#od_1997_filtrada <- od_1997_filtrada |>
+#    mutate(
+#        GRAU_INS_ANTIGO = GRAU_INS,
+#       GRAU_INS = case_when(
+#           GRAU_INS %in% c(1L, 2L, 3L) ~ 1L, # não alfabetizado / pré-escola / fund. incompleto
+#           GRAU_INS == 4L ~ 2L, # fund. completo
+#           GRAU_INS == 5L ~ 3L, # médio incompleto
+#           GRAU_INS %in% c(6L, 7L) ~ 4L, # médio completo / superior incompleto
+#           GRAU_INS == 8L ~ 5L, # superior completo
+#           TRUE ~ NA_integer_
+#      )
+#   )
+
+##### Fazendo uma variável indicadora se a viagem dos indivíduos foi realizada ao centro expandido de SP #####
+
+centro_exp_shp <- st_read('SIRGAS_SHP_restricaoveiculomian.shp')
+
+centro_exp_shp <- centro_exp_shp |>
+    st_set_crs(31983)
+
+#### Transformando as coordenadas da base de 2007 de SAD69 para SIRGAS ####
+
+## Coordenadas de origem da viagem ##
+
+od_2007_filtrada <- od_2007_filtrada |>
+    mutate(id_temp_row = row_number())
+
+od_2007_coord_origem <- od_2007_filtrada |>
+    filter(!is.na(CO_O_X) & !is.na(CO_O_Y)) |>
+    select(id_temp_row, CO_O_X, CO_O_Y) |>
+    st_as_sf(coords = c('CO_O_X', 'CO_O_Y'), crs = 29193, remove = FALSE) |>
+    st_transform(crs = 31983) |>
     mutate(
-        GRAU_INS_ANTIGO = GRAU_INS,
-        GRAU_INS = case_when(
-            GRAU_INS %in% c(1L, 2L, 3L) ~ 1L, # não alfabetizado / pré-escola / fund. incompleto
-            GRAU_INS == 4L ~ 2L, # fund. completo
-            GRAU_INS == 5L ~ 3L, # médio incompleto
-            GRAU_INS %in% c(6L, 7L) ~ 4L, # médio completo / superior incompleto
-            GRAU_INS == 8L ~ 5L, # superior completo
-            TRUE ~ NA_integer_
+        CO_O_X_SIRGAS = st_coordinates(geometry)[, 1],
+        CO_O_Y_SIRGAS = st_coordinates(geometry)[, 2]
+    ) |>
+    st_drop_geometry() |>
+    select(id_temp_row, CO_O_X_SIRGAS, CO_O_Y_SIRGAS)
+
+od_2007_filtrada <- od_2007_filtrada |>
+    left_join(od_2007_coord_origem, by = 'id_temp_row')
+
+## Coordenadas de destino da viagem ##
+
+od_2007_filtrada <- od_2007_filtrada |>
+    mutate(id_temp_row = row_number())
+
+od_2007_coord_destino <- od_2007_filtrada |>
+    filter(!is.na(CO_D_X) & !is.na(CO_D_Y)) |>
+    select(id_temp_row, CO_D_X, CO_D_Y) |>
+    st_as_sf(coords = c('CO_D_X', 'CO_D_Y'), crs = 29193, remove = FALSE) |>
+    st_transform(crs = 31983) |>
+    mutate(
+        CO_D_X_SIRGAS = st_coordinates(geometry)[, 1],
+        CO_D_Y_SIRGAS = st_coordinates(geometry)[, 2]
+    ) |>
+    st_drop_geometry() |>
+    select(id_temp_row, CO_D_X_SIRGAS, CO_D_Y_SIRGAS)
+
+od_2007_filtrada <- od_2007_filtrada |>
+    left_join(od_2007_coord_destino, by = 'id_temp_row')
+
+#### Criando variável indicadora do destino da viagem ser no centro expandido ####
+
+### 2007 ###
+
+bool_centro_exp_07 <- od_2007_filtrada |>
+    filter(!is.na(CO_D_X_SIRGAS) & !is.na(CO_D_Y_SIRGAS)) |>
+    select(id_temp_row, CO_D_X_SIRGAS, CO_D_Y_SIRGAS) |>
+    st_as_sf(
+        coords = c('CO_D_X_SIRGAS', 'CO_D_Y_SIRGAS'),
+        crs = 31983,
+        remove = FALSE
+    ) |>
+    mutate(
+        bool_destino_centro_exp = lengths(st_intersects(
+            geometry,
+            centro_exp_shp
+        )) >
+            0
+    ) |>
+    st_drop_geometry() |>
+    select(id_temp_row, bool_destino_centro_exp)
+
+od_2007_filtrada <- od_2007_filtrada |>
+    left_join(bool_centro_exp_07, by = 'id_temp_row') |>
+    mutate(
+        indic_destino_centro_exp = ifelse(
+            is.na(bool_destino_centro_exp),
+            0,
+            ifelse(bool_destino_centro_exp == TRUE, 1, 0)
         )
-    )
+    ) |>
+    select(-id_temp_row)
 
+### 2017 ###
 
-##### Unindo as bases filtradas #####
+od_2017_filtrada <- od_2017_filtrada |>
+    mutate(id_temp_row = row_number())
+
+bool_centro_exp_17 <- od_2017_filtrada |>
+    filter(!is.na(CO_D_X_SIRGAS) & !is.na(CO_D_Y_SIRGAS)) |>
+    select(id_temp_row, CO_D_X_SIRGAS, CO_D_Y_SIRGAS) |>
+    st_as_sf(
+        coords = c('CO_D_X_SIRGAS', 'CO_D_Y_SIRGAS'),
+        crs = 31983,
+        remove = FALSE
+    ) |>
+    mutate(
+        bool_destino_centro_exp = lengths(st_intersects(
+            geometry,
+            centro_exp_shp
+        )) >
+            0
+    ) |>
+    st_drop_geometry() |>
+    select(id_temp_row, bool_destino_centro_exp)
+
+od_2017_filtrada <- od_2017_filtrada |>
+    left_join(bool_centro_exp_17, by = 'id_temp_row') |>
+    mutate(
+        indic_destino_centro_exp = ifelse(
+            is.na(bool_destino_centro_exp),
+            0,
+            ifelse(bool_destino_centro_exp == TRUE, 1, 0)
+        )
+    ) |>
+    select(-id_temp_row)
+
+### 2023 ###
+
+od_2023_filtrada <- od_2023_filtrada |>
+    mutate(id_temp_row = row_number())
+
+bool_centro_exp_23 <- od_2023_filtrada |>
+    filter(!is.na(CO_D_X_SIRGAS) & !is.na(CO_D_Y_SIRGAS)) |>
+    select(id_temp_row, CO_D_X_SIRGAS, CO_D_Y_SIRGAS) |>
+    st_as_sf(
+        coords = c('CO_D_X_SIRGAS', 'CO_D_Y_SIRGAS'),
+        crs = 31983,
+        remove = FALSE
+    ) |>
+    mutate(
+        bool_destino_centro_exp = lengths(st_intersects(
+            geometry,
+            centro_exp_shp
+        )) >
+            0
+    ) |>
+    st_drop_geometry() |>
+    select(id_temp_row, bool_destino_centro_exp)
+
+od_2023_filtrada <- od_2023_filtrada |>
+    left_join(bool_centro_exp_23, by = 'id_temp_row') |>
+    mutate(
+        indic_destino_centro_exp = ifelse(
+            is.na(bool_destino_centro_exp),
+            0,
+            ifelse(bool_destino_centro_exp == TRUE, 1, 0)
+        )
+    ) |>
+    select(-id_temp_row)
+
+##### Unindo todas as bases filtradas #####
 
 od_completa <- bind_rows(
+    #od_1997_filtrada,
     od_2007_filtrada,
     od_2017_filtrada,
     od_2023_filtrada
 )
-
-##### Fazendo uma variável indicadora se a viagem dos indivíduos foi realizada ao centro expandido de SP #####
