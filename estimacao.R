2a9476a0bec475f1d44e47a67470618e15519303
phiLdias25 <philipe.dias03@gmail.com>
2025-12-27 20:52:01 -0300

    Criação de estatísticas descritivas dos grupos de tratado e controles e estimação com todos os controles

--- estimacao.R
+++ estimacao.R
@@ -9,11 +9,537 @@ library(rio)
 library(fixest)
 library(did)
 library(bacondecomp)
-library(modelsummary)
 
 ##### Abrindo bases de dados #####
 
 base_matching <- import('base_pareamento.dbf')
+base_linhas_futuras <- import('base_linhas_futuras.dbf')
+base_cptm <- import('base_cptm.dbf')
+
+##### Criando as variáveis de pré e pós e realizando a estimação com o grupo de controle 1 - Linhas Futuras #####
+
+base_reg_linhas <- base_linhas_futuras |>
+  mutate(
+    post = if_else(ano >= 2018, 1, 0)
+  )
+
+### Média simples do efeito ###
+
+did_agregado_linhas_dest <- feols(
+  indic_dest ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_dest)
+
+did_agregado_linhas_trab <- feols(
+  trab_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_trab)
+
+did_agregado_linhas_educ <- feols(
+  educ_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_educ)
+
+did_agregado_linhas_saude <- feols(
+  saude_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_saude)
+
+did_agregado_linhas_lazer <- feols(
+  lazer_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_lazer)
+
+did_agregado_linhas_comp <- feols(
+  comp_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_comp)
+
+did_agregado_linhas_emp <- feols(
+  emp_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_emp)
+
+did_agregado_linhas_trabeducem <- feols(
+  trabeducem ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_trabeducem)
+
+did_agregado_linhas_trabemp <- feols(
+  trabemp ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_trabemp)
+
+did_agregado_linhas_trabeduc <- feols(
+  trabeduc ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_trabeduc)
+
+did_agregado_linhas_lazcompsa <- feols(
+  lazcompsa ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_lazcompsa)
+
+did_agregado_linhas_lazcomp <- feols(
+  lazcomp ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_linhas_lazcomp)
+
+### Event Study ###
+
+event_study_linhas_dest <- feols(
+  indic_dest ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_dest)
+
+event_study_linhas_trab <- feols(
+  trab_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+etable(event_study_linhas_trab)
+
+event_study_linhas_educ <- feols(
+  educ_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_educ)
+
+event_study_linhas_saude <- feols(
+  saude_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_saude)
+
+event_study_linhas_lazer <- feols(
+  lazer_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_lazer)
+
+event_study_linhas_comp <- feols(
+  comp_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_comp)
+
+event_study_linhas_emp <- feols(
+  emp_cen ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_emp)
+
+event_study_linhas_trabeducem <- feols(
+  trabeducem ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_trabeducem)
+
+event_study_linhas_trabemp <- feols(
+  trabemp ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_trabemp)
+
+event_study_linhas_trabeduc <- feols(
+  trabeduc ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_trabeduc)
+
+event_study_linhas_lazcompsa <- feols(
+  lazcompsa ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_lazcompsa)
+
+event_study_linhas_lazcomp <- feols(
+  lazcomp ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_linhas,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_linhas_lazcomp)
+
+
+##### Criando as variáveis de pré e pós e realizando a estimação com o grupo de controle 2 - CPTM #####
+
+base_reg_cptm <- base_cptm |>
+  mutate(
+    post = if_else(ano >= 2018, 1, 0)
+  )
+
+### Média simples do efeito ###
+
+did_agregado_cptm_dest <- feols(
+  indic_dest ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_dest)
+
+did_agregado_cptm_trab <- feols(
+  trab_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_trab)
+
+did_agregado_cptm_educ <- feols(
+  educ_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_educ)
+
+did_agregado_cptm_saude <- feols(
+  saude_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_saude)
+
+did_agregado_cptm_lazer <- feols(
+  lazer_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_lazer)
+
+did_agregado_cptm_comp <- feols(
+  comp_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_comp)
+
+did_agregado_cptm_emp <- feols(
+  emp_cen ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_emp)
+
+did_agregado_cptm_trabeducem <- feols(
+  trabeducem ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_trabeducem)
+
+did_agregado_cptm_trabemp <- feols(
+  trabemp ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_trabemp)
+
+did_agregado_cptm_trabeduc <- feols(
+  trabeduc ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_trabeduc)
+
+did_agregado_cptm_lazcompsa <- feols(
+  lazcompsa ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_lazcompsa)
+
+did_agregado_cptm_lazcomp <- feols(
+  lazcomp ~ tratamento * post + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(did_agregado_cptm_lazcomp)
+
+### Event Study ###
+
+event_study_cptm_dest <- feols(
+  indic_dest ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_dest)
+
+event_study_cptm_trab <- feols(
+  trab_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+etable(event_study_cptm_trab)
+
+event_study_cptm_educ <- feols(
+  educ_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_educ)
+
+event_study_cptm_saude <- feols(
+  saude_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_saude)
+
+event_study_cptm_lazer <- feols(
+  lazer_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_lazer)
+
+event_study_cptm_comp <- feols(
+  comp_cen ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_comp)
+
+event_study_cptm_emp <- feols(
+  emp_cen ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_emp)
+
+event_study_cptm_trabeducem <- feols(
+  trabeducem ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_trabeducem)
+
+event_study_cptm_trabemp <- feols(
+  trabemp ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_trabemp)
+
+event_study_cptm_trabeduc <- feols(
+  trabeduc ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_trabeduc)
+
+event_study_cptm_lazcompsa <- feols(
+  lazcompsa ~ i(ano, tratamento, ref = 2017) +
+    IDADE +
+    SEXO +
+    GRAU_INS +
+    CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_lazcompsa)
+
+event_study_cptm_lazcomp <- feols(
+  lazcomp ~ i(ano, tratamento, ref = 2017) + IDADE + SEXO + GRAU_INS + CD_ATIVI,
+  data = base_reg_cptm,
+
+  cluster = ~ZONA_O
+)
+
+etable(event_study_cptm_lazcomp)
 
 ##### Criando as variáveis de pré e pós e realizando a estimação com o grupo de controle pareado #####
 
@@ -22,7 +548,7 @@ base_reg_matching <- base_matching |>
     post = if_else(ano >= 2018, 1, 0)
   )
 
-#### Média simples do efeito ####
+### Média simples do efeito ###
 
 did_agregado_match_dest <- feols(
   indic_dest ~ tratamento * post,
@@ -132,7 +658,7 @@ did_agregado_match_lazcomp <- feols(
 
 etable(did_agregado_match_lazcomp)
 
-#### Event Study ####
+### Event Study ###
 
 event_study_match_dest <- feols(
   indic_dest ~ i(ano, tratamento, ref = 2017) +
--- matching.R
+++ matching.R
@@ -115,6 +115,141 @@ base_completa_match <- match.data(match)
 
 export(base_completa_match, 'base_pareamento.dbf')
 
+##### Fazendo os outros grupos de controle paramétricos #####
+
+#### Grupo de controle paramétrico 1 - Linhas Futuras ####
+
+od_linhas_futuras <- od_completa |>
+  filter(tipo_grupo != 'Morador_Centro_Exp') |>
+  filter(tipo_grupo != 'Candidatos_Controle_MatchIt') |>
+  filter(tipo_grupo != 'Controle_CPTM') |>
+  mutate(tratamento_binario = ifelse(tipo_grupo == 'Tratamento', 1, 0)) |>
+  select(
+    tipo_grupo,
+    tratamento_binario,
+    IDADE,
+    SEXO,
+    GRAU_INS,
+    CD_ATIVI,
+    VL_REN_I_D,
+    TIPVG,
+    ano,
+    indic_dest,
+    trab_cen,
+    educ_cen,
+    saude_cen,
+    lazer_cen,
+    comp_cen,
+    emp_cen,
+    trabeducemp,
+    trabemp,
+    lazcompsa,
+    lazcomp,
+    trabeduc,
+    ZONA_O
+  ) |>
+  mutate(
+    SEXO = case_when(
+      SEXO == 1 ~ 'Masculino',
+      SEXO == 2 ~ 'Feminino',
+      TRUE ~ NA_character_
+    ),
+    SEXO = as.factor(SEXO),
+    GRAU_INS = case_when(
+      GRAU_INS == 1 ~ 'Não alfabetizado/ Fund 1 incompleto',
+      GRAU_INS == 2 ~ 'Fund 1 completo/ Fund 2 incompleto',
+      GRAU_INS == 3 ~ 'Fund 2 completo/ Médio incompleto',
+      GRAU_INS == 4 ~ 'Médio completo/ Superior incompleto',
+      GRAU_INS == 5 ~ 'Superior completo',
+      TRUE ~ NA_character_
+    ),
+    GRAU_INS = as.factor(GRAU_INS),
+    CD_ATIVI = case_when(
+      CD_ATIVI == 1 ~ 'Trabalho Regular',
+      CD_ATIVI == 2 ~ 'Bico',
+      CD_ATIVI == 3 ~ 'Licença Médica',
+      CD_ATIVI == 4 ~ 'Aposentado/Pensionista',
+      CD_ATIVI == 5 ~ 'Desempregado',
+      CD_ATIVI == 6 ~ 'Nunca Trabalhou',
+      CD_ATIVI == 7 ~ 'Dona de Casa',
+      CD_ATIVI == 8 ~ 'Estudante',
+      TRUE ~ NA_character_
+    ),
+    CD_ATIVI = as.factor(CD_ATIVI)
+  ) |>
+  mutate(VIAGEM_TRANS_PUBL = ifelse(TIPVG == 1, 1, 0)) |>
+  drop_na()
+
+base_controle_linhas_futuras <- export(
+  od_linhas_futuras,
+  'base_linhas_futuras.dbf'
+)
+
+#### Grupo de controle paramétrico 2 - CPTM ####
+
+od_cptm <- od_completa |>
+  filter(tipo_grupo != 'Morador_Centro_Exp') |>
+  filter(tipo_grupo != 'Candidatos_Controle_MatchIt') |>
+  filter(tipo_grupo != 'Controle_Linhas_Futuras') |>
+  mutate(tratamento_binario = ifelse(tipo_grupo == 'Tratamento', 1, 0)) |>
+  select(
+    tipo_grupo,
+    tratamento_binario,
+    IDADE,
+    SEXO,
+    GRAU_INS,
+    CD_ATIVI,
+    VL_REN_I_D,
+    TIPVG,
+    ano,
+    indic_dest,
+    trab_cen,
+    educ_cen,
+    saude_cen,
+    lazer_cen,
+    comp_cen,
+    emp_cen,
+    trabeducemp,
+    trabemp,
+    lazcompsa,
+    lazcomp,
+    trabeduc,
+    ZONA_O
+  ) |>
+  mutate(
+    SEXO = case_when(
+      SEXO == 1 ~ 'Masculino',
+      SEXO == 2 ~ 'Feminino',
+      TRUE ~ NA_character_
+    ),
+    SEXO = as.factor(SEXO),
+    GRAU_INS = case_when(
+      GRAU_INS == 1 ~ 'Não alfabetizado/ Fund 1 incompleto',
+      GRAU_INS == 2 ~ 'Fund 1 completo/ Fund 2 incompleto',
+      GRAU_INS == 3 ~ 'Fund 2 completo/ Médio incompleto',
+      GRAU_INS == 4 ~ 'Médio completo/ Superior incompleto',
+      GRAU_INS == 5 ~ 'Superior completo',
+      TRUE ~ NA_character_
+    ),
+    GRAU_INS = as.factor(GRAU_INS),
+    CD_ATIVI = case_when(
+      CD_ATIVI == 1 ~ 'Trabalho Regular',
+      CD_ATIVI == 2 ~ 'Bico',
+      CD_ATIVI == 3 ~ 'Licença Médica',
+      CD_ATIVI == 4 ~ 'Aposentado/Pensionista',
+      CD_ATIVI == 5 ~ 'Desempregado',
+      CD_ATIVI == 6 ~ 'Nunca Trabalhou',
+      CD_ATIVI == 7 ~ 'Dona de Casa',
+      CD_ATIVI == 8 ~ 'Estudante',
+      TRUE ~ NA_character_
+    ),
+    CD_ATIVI = as.factor(CD_ATIVI)
+  ) |>
+  mutate(VIAGEM_TRANS_PUBL = ifelse(TIPVG == 1, 1, 0)) |>
+  drop_na()
+
+base_controle_cptm <- export(od_cptm, 'base_cptm.dbf')
+
 ##### Fazendo o diagnóstico do pareamento #####
 
 tabela_comparativa <- bal.tab(match, un = TRUE, thresholds = c(m = .1))
@@ -136,10 +271,10 @@ print(plot_comparativo)
 
 ##### Analisando estatísticas descritivas dos grupos de controle criados, tanto os paramétricos quanto o por pareamento #####
 
-#### Grupo de controle paramétrico ####
+#### Grupo de controle 1 - Linhas Futuras ####
 
-od_estat_par <- od_completa |>
-  filter(tipo_grupo == 'Controle_Parametrico') |>
+od_estat_linhas <- od_completa |>
+  filter(tipo_grupo == 'Controle_Linhas_Futuras') |>
   select(IDADE, SEXO, GRAU_INS, CD_ATIVI, VL_REN_I_D, TIPVG) |>
   mutate(
     SEXO = case_when(
@@ -180,7 +315,7 @@ od_estat_par <- od_completa |>
   ) |>
   drop_na()
 
-idade_anual_par <- od_estat_par |>
+idade_anual_linhas <- od_estat_linhas |>
   summarise(
     Valor = mean(IDADE, na.rm = TRUE),
     Variavel = 'Idade Média (Anos)'
@@ -189,7 +324,7 @@ idade_anual_par <- od_estat_par |>
   ungroup() |>
   select(Variavel, Valor)
 
-categorias_anual_par <- od_estat_par |>
+categorias_anual_linhas <- od_estat_linhas |>
   select(-IDADE) |>
   pivot_longer(
     cols = c(SEXO, GRAU_INS, CD_ATIVI, TIPVG),
@@ -206,7 +341,7 @@ categorias_anual_par <- od_estat_par |>
   ungroup() |>
   select(Variavel = categoria, Valor)
 
-renda_anual_par <- od_estat_par |>
+renda_anual_linhas <- od_estat_linhas |>
   filter(!is.na(VL_REN_I_D)) |>
   summarise(
     Valor = mean(VL_REN_I_D, na.rm = TRUE),
@@ -216,10 +351,10 @@ renda_anual_par <- od_estat_par |>
   mutate(Valor = round(Valor, 2)) |>
   select(Variavel, Valor)
 
-tabela_descrit_par <- bind_rows(
-  idade_anual_par,
-  renda_anual_par,
-  categorias_anual_par
+tabela_descrit_linhas <- bind_rows(
+  idade_anual_linhas,
+  renda_anual_linhas,
+  categorias_anual_linhas
 ) |>
   arrange(factor(
     Variavel,
@@ -248,11 +383,134 @@ tabela_descrit_par <- bind_rows(
     )
   ))
 
-tabela_latex_par <- tabela_descrit_par |>
+tabela_latex_linhas <- tabela_descrit_linhas |>
   kbl(
     format = 'latex',
     booktabs = TRUE,
-    label = 'tab:descritivas_parametrico'
+    label = 'tab:descritivas_linhas_futuras'
+  ) |>
+  kable_styling(
+    latex_options = c('striped', 'hold_position'),
+    position = 'center'
+  )
+
+#### Grupo de controle 2 - CPTM ####
+
+od_estat_cptm <- od_completa |>
+  filter(tipo_grupo == 'Controle_CPTM') |>
+  select(IDADE, SEXO, GRAU_INS, CD_ATIVI, VL_REN_I_D, TIPVG) |>
+  mutate(
+    SEXO = case_when(
+      SEXO == 1 ~ 'Masculino',
+      SEXO == 2 ~ 'Feminino',
+      TRUE ~ NA_character_
+    ),
+    SEXO = as.factor(SEXO),
+    GRAU_INS = case_when(
+      GRAU_INS == 1 ~ 'Não alfabetizado/ Fund 1 incompleto',
+      GRAU_INS == 2 ~ 'Fund 1 completo/ Fund 2 incompleto',
+      GRAU_INS == 3 ~ 'Fund 2 completo/ Médio incompleto',
+      GRAU_INS == 4 ~ 'Médio completo/ Superior incompleto',
+      GRAU_INS == 5 ~ 'Superior completo',
+      TRUE ~ NA_character_
+    ),
+    GRAU_INS = as.factor(GRAU_INS),
+    CD_ATIVI = case_when(
+      CD_ATIVI == 1 ~ 'Trabalho Regular',
+      CD_ATIVI == 2 ~ 'Bico',
+      CD_ATIVI == 3 ~ 'Licença Médica',
+      CD_ATIVI == 4 ~ 'Aposentado/Pensionista',
+      CD_ATIVI == 5 ~ 'Desempregado',
+      CD_ATIVI == 6 ~ 'Nunca Trabalhou',
+      CD_ATIVI == 7 ~ 'Dona de Casa',
+      CD_ATIVI == 8 ~ 'Estudante',
+      TRUE ~ NA_character_
+    ),
+    CD_ATIVI = as.factor(CD_ATIVI),
+    TIPVG = case_when(
+      TIPVG == 1 ~ 'Coletivo',
+      TIPVG == 2 ~ 'Particular',
+      TIPVG == 3 ~ 'A pé',
+      TIPVG == 4 ~ 'Bicicleta',
+      TRUE ~ NA_character_
+    ),
+    TIPVG = as.factor(TIPVG)
+  ) |>
+  drop_na()
+
+idade_anual_cptm <- od_estat_cptm |>
+  summarise(
+    Valor = mean(IDADE, na.rm = TRUE),
+    Variavel = 'Idade Média (Anos)'
+  ) |>
+  mutate(Valor = round(Valor, 1)) |>
+  ungroup() |>
+  select(Variavel, Valor)
+
+categorias_anual_cptm <- od_estat_cptm |>
+  select(-IDADE) |>
+  pivot_longer(
+    cols = c(SEXO, GRAU_INS, CD_ATIVI, TIPVG),
+    names_to = 'origem',
+    values_to = 'categoria'
+  ) |>
+  filter(!is.na(categoria)) |>
+  count(origem, categoria) |>
+  group_by(origem) |>
+  mutate(
+    prob = n / sum(n),
+    Valor = round(prob, 3)
+  ) |>
+  ungroup() |>
+  select(Variavel = categoria, Valor)
+
+renda_anual_cptm <- od_estat_cptm |>
+  filter(!is.na(VL_REN_I_D)) |>
+  summarise(
+    Valor = mean(VL_REN_I_D, na.rm = TRUE),
+    Variavel = 'Renda Individual Média (R$)'
+  ) |>
+  ungroup() |>
+  mutate(Valor = round(Valor, 2)) |>
+  select(Variavel, Valor)
+
+tabela_descrit_cptm <- bind_rows(
+  idade_anual_cptm,
+  renda_anual_cptm,
+  categorias_anual_cptm
+) |>
+  arrange(factor(
+    Variavel,
+    levels = c(
+      'Idade Média (Anos)',
+      'Renda Individual Média (R$)',
+      'Masculino',
+      'Feminino',
+      'Não alfabetizado/ Fund 1 incompleto',
+      'Fund 1 completo/ Fund 2 incompleto',
+      'Fund 2 completo/ Médio incompleto',
+      'Médio completo/ Superior incompleto',
+      'Superior completo',
+      'Trabalho Regular',
+      'Bico',
+      'Licença Médica',
+      'Aposentado/Pensionista',
+      'Desempregado',
+      'Nunca Trabalhou',
+      'Dona de Casa',
+      'Estudante',
+      'Coletivo',
+      'Particular',
+      'A pé',
+      'Bicicleta'
+    )
+  ))
+
+tabela_latex_cptm <- tabela_descrit_cptm |>
+  kbl(
+    format = 'latex',
+    booktabs = TRUE,
+    label = 'tab:descritivas_cptm'
   ) |>
   kable_styling(
     latex_options = c('striped', 'hold_position'),
